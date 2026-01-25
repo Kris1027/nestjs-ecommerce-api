fa87bf42b563c713bd824dafc4c695b33fbe8d25
kris1027 <krzys89@gmail.com>
2026-01-25 13:07:30 +0100

    feat: add request ID tracing and move TransformInterceptor to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER, APP_GUARD, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { ZodValidationPipe } from 'nestjs-zod';
 import { LoggerModule } from 'nestjs-pino';
@@ -9,6 +9,9 @@ import { AppService } from './app.service';
 import { validate, env } from './config/env.validation';
 import { PrismaModule } from './prisma/prisma.module';
 import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
+import { IncomingMessage } from 'http';
+import { randomUUID } from 'crypto';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 @Module({
   imports: [
@@ -44,6 +47,15 @@ import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
         // Log level based on environment
         level: env.NODE_ENV === 'production' ? 'info' : 'debug',
 
+        // Generate request ID for distributed tracing
+        genReqId: (req: IncomingMessage) => {
+          const existingId = req.headers['x-request-id'];
+          if (existingId) {
+            return existingId as string;
+          }
+          return randomUUID();
+        },
+
         // Pretty print in development, JSON in production
         transport:
           env.NODE_ENV !== 'production'
@@ -63,6 +75,7 @@ import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
           res: 'response',
           err: 'error',
           responseTime: 'duration',
+          reqId: 'requestId',
         },
 
         // Don't log request/response bodies (security + performance)
@@ -95,6 +108,10 @@ import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
     {
       provide: APP_FILTER,
       useClass: GlobalExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
     },
   ],
 })
